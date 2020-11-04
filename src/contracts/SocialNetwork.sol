ea719103d509b25cdbdadf1e5e83b361903d866f
Aaron Bruce <aaronbruce555@gmail.com>
2020-11-04 16:56:46 -0500

    Add success and fail cases to create posts and Update createPosts function

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -5,7 +5,7 @@ contract SocialNetwork {
   uint public postCount = 0;
   mapping(uint => Post) posts;  // key: value pair {uint: Post} for posts
 
-
+  // Model the structure of a Post
   struct Post {
     uint id;
     string content;
@@ -13,15 +13,27 @@ contract SocialNetwork {
     address author;
   }
 
+  // Event to be triggered on call external consumers can subscribe to these events
+  event PostCreated(
+    uint id,
+    string content,
+    uint tipAmount,
+    address author
+  );
+
   constructor() public {
     name = "Dapp University Social Network Tutorial";
   }
 
   function createPost(string memory _content) public {
+    // Require valid content
+    require(bytes(_content).length > 0);
     postCount++;  // increment post count
-    _posts = Post(postCount, _content, 0, msg.sender); // create a Post using the struct
-    posts[postCount] = _post; // assign the new Post to the mapping
-    // posts[postCount] = Post(postCount, _content, 0, msg.sender) // one-line solution for previous 2 lines
+    // _post = Post(postCount, _content, 0, msg.sender); // create a Post using the struct
+    // posts[postCount] = _post; // assign the new Post to the mapping
+    posts[postCount] = Post(postCount, _content, 0, msg.sender); // one-line solution for previous 2 lines
+
+    emit PostCreated(postCount, _content, 0, msg.sender);
   }
 
 
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -5,7 +5,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-  contract('SocialNetwork', (accounts) => {
+  contract('SocialNetwork', ([deployer, author, tipper]) => {
     let socialNetwork
 
     before(async () => {
@@ -28,15 +28,27 @@ require('chai')
     })
 
     describe('posts', async () => {
-
+      let result, postCount;
       it('creates posts', async () => {
-        
+        result = await socialNetwork.createPost('This is my first post', { from: author });
+        postCount = await socialNetwork.postCount();
+        // SUCCESS
+        assert.equal(postCount, 1);
+        // Trigger Event
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct');
+        assert.equal(event.content, "This is my first post", 'content is correct');
+        assert.equal(event.tipAmount, '0', 'tip amount is correct');
+        assert.equal(event.author, author, 'author is correct');
+
+        // FAILURE
+        await socialNetwork.createPost('', { from: author}).should.be.rejected;
       })
       it('lists posts', async () => {
-
+        assert.fail("Empty Test")
       })
       it('allows users to tip posts', async () => {
-
+        assert.fail("Empty Test")
       })
     })
 
