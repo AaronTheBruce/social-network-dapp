95dc5ffee9de9a3b771e4dfca2540e3492330b5a
Aaron Bruce <aaronbruce555@gmail.com>
2020-11-04 15:19:34 -0500

    Add tests for SocialNetwork and remigrated truffle

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,35 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
   string public name;
+  uint public postCount = 0;
+  mapping(uint => Post) posts;  // key: value pair {uint: Post} for posts
+
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address author;
+  }
 
   constructor() public {
     name = "Dapp University Social Network Tutorial";
   }
+
+  function createPost(string memory _content) public {
+    postCount++;  // increment post count
+    _posts = Post(postCount, _content, 0, msg.sender); // create a Post using the struct
+    posts[postCount] = _post; // assign the new Post to the mapping
+    // posts[postCount] = Post(postCount, _content, 0, msg.sender) // one-line solution for previous 2 lines
+  }
+
+
+
+
+
+  /**
+    Must create Posts
+    List all posts
+    Tip Posts
+   */
 }
---
+++ test/SocialNetwork.js
@@ -0,0 +1,43 @@
+const { assert } = require('chai');
+
+const SocialNetwork = artifacts.require('./SocialNetwork.sol');
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+  contract('SocialNetwork', (accounts) => {
+    let socialNetwork
+
+    before(async () => {
+      socialNetwork = await SocialNetwork.deployed()
+    })
+
+    describe('deployment', () => {
+      it('deploys successfully', async () => {
+        const address = await SocialNetwork.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+      })
+
+      it('has a name', async () => {
+        const name = await socialNetwork.name()
+        assert.equal(name, 'Dapp University Social Network Tutorial');
+      })
+    })
+
+    describe('posts', async () => {
+
+      it('creates posts', async () => {
+        
+      })
+      it('lists posts', async () => {
+
+      })
+      it('allows users to tip posts', async () => {
+
+      })
+    })
+
+  })
